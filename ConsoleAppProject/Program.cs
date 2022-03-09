cb1c5e8e3b158fb766bed080f73444423f9a0b83
Nackshayan Mehanathan <22043338@bucks.ac.uk>
2022-03-09 12:47:01 +0000

    ADD

--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -1,6 +1,4 @@
 ﻿using ConsoleAppProject.App01;
-using ConsoleAppProject.App03;
-using ConsoleAppProject.Helpers;
 using System;
 
 namespace ConsoleAppProject
@@ -8,10 +6,10 @@ namespace ConsoleAppProject
     /// <summary>
     /// The main method in this class is called first
     /// when the application is started.  It will be used
-    /// to start App01 to App05 for CO453 CW1
+    /// to start Apps 01 to 05 for CO453 CW1
     /// 
     /// This Project has been modified by:
-    /// Derek Peacock 05/02/2022
+    /// Nackshayan 08/03/2022
     /// </summary>
     public static class Program
     {
@@ -19,15 +17,12 @@ namespace ConsoleAppProject
         {
             Console.ForegroundColor = ConsoleColor.Yellow;
 
+            Console.WriteLine("BNU CO453 Applications Programming2021-2022!");
             Console.WriteLine();
-            Console.WriteLine(" =================================================");
-            Console.WriteLine("    BNU CO453 Applications Programming 2021-2022! ");
-            Console.WriteLine("        by Derek                                  ");
-            Console.WriteLine(" =================================================");
-            Console.WriteLine();
+            Console.Beep();
 
             DistanceConverter converter = new DistanceConverter();
-            converter.Run();
+            converter.ConvertDistance();
         }
     }
 }
